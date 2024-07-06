5b9ac5ace7538f535e105751ebafb048ddbf3b30
UmangJ3 <haharofl14@gmail.com>
2024-07-06 22:14:38 +0530

    Reducing CCN in Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,45 +1,49 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
 
-#include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
 
 char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
+    switch (toupper(c)) {
         case 'B': case 'F': case 'P': case 'V': return '1';
         case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
         case 'D': case 'T': return '3';
         case 'L': return '4';
         case 'M': case 'N': return '5';
         case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
+        default: return '0';
     }
 }
 
-char getFirstLetter(const char *name) {
+char extractAndCapitalizeInitial(const char *name) {
     return toupper(name[0]);
 }
 
-void padWithZeros(char *soundex, int &index) {
+void fillWithZeros(char *soundex, int &index) {
     while (index < 4) {
         soundex[index++] = '0';
     }
     soundex[4] = '\0';
 }
-
-bool isDuplicateCode(char currentCode, char lastCode) {
-    return currentCode == lastCode && currentCode != '0';
+bool isCodeDuplicate(char currentCode, char previousCode) {
+    return currentCode == previousCode && currentCode != '0';
 }
 
-void encodeRemainingLetters(const char *name, char *soundex, int &index, char &lastCode) {
-    for (int i = 1; name[i] != '\0' && index < 4; i++) {
+void appendSoundexCode(char *soundex, int &index, char code) {
+    if (index < 4) {
+        soundex[index++] = code;
+    }
+}
+
+// Function to process and encode the remaining letters of the name
+void processRemainingLetters(const char *name, char *soundex, int &index, char &lastCode) {
+    for (int i = 1; name[i] != '\0'; i++) {
         char currentCode = getSoundexCode(name[i]);
-        if (!isDuplicateCode(currentCode, lastCode)) {
-            if (currentCode != '0') {
-                soundex[index++] = currentCode;
-            }
+        if (!isCodeDuplicate(currentCode, lastCode) && currentCode != '0') {
+            appendSoundexCode(soundex, index, currentCode);
+        }
+        if (currentCode != '0') {
             lastCode = currentCode;
         }
     }
@@ -51,11 +55,13 @@ void generateSoundex(const char *name, char *soundex) {
         return;
     }
 
-    soundex[0] = getFirstLetter(name);
+    soundex[0] = extractAndCapitalizeInitial(name);
     int index = 1;
     char lastCode = getSoundexCode(name[0]);
 
-    encodeRemainingLetters(name, soundex, index, lastCode);
-    padWithZeros(soundex, index);
+    processRemainingLetters(name, soundex, index, lastCode);
+    fillWithZeros(soundex, index);
 }
+
 #endif // SOUNDEX_H
+
